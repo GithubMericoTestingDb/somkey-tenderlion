6d79e6f745d9479c44f7bf4813a54bd728d2bfef
Carlos Ramirez <qadbtester4@gmail.com>
2020-12-15 16:16:49 -0800

    Added test 19

---
+++ src/test/java/com/tests/_19_repo_test_1.java
@@ -0,0 +1,41 @@
+package com.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class _19_repo_test_1 {
+
+    private WebDriver driver;
+
+    public static void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void testingRepoCommitsOne(){
+
+        driver = new ChromeDriver();
+
+        driver.get("https://eviltester.github.io/synchole/collapseable.html");
+
+        Assertions.assertEquals("EvilTester",
+                driver.findElement(
+                        By.cssSelector("h3")).getText());
+    }
+}
